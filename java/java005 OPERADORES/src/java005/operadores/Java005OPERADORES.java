662723e0eccc3313b147f8ca81e5a4488ff987e5
yago <100420066+yagodiax@users.noreply.github.com>
2025-02-22 11:37:13 -0300

    operadores

---
+++ java/java005 OPERADORES/src/java005/operadores/Java005OPERADORES.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package java005.operadores;
+
+/**
+ *
+ * @author yagod
+ */
+public class Java005OPERADORES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int n1 = 3;
+        int n2 = 5;
+        float m = (n1 + n2)/2;
+        System.out.println("A média é igual a: " + m);
+    }
+    
+}
