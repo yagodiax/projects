ad2810b9ae5797da4b7c6fcd553e0217a8e14022
yago <100420066+yagodiax@users.noreply.github.com>
2025-02-23 12:08:32 -0300

    Operadores Logicos e Relacionais em Java

---
+++ java/java007 OPERADORES LOGICOS E RELACIONAIS/src/java007/operadores/logicos/e/relacionais/Java007OPERADORESLOGICOSERELACIONAIS.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package java007.operadores.logicos.e.relacionais;
+
+/**
+ *
+ * @author yagod
+ */
+public class Java007OPERADORESLOGICOSERELACIONAIS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int n1, n2, r;
+        n1 = 14;
+        n2 = 18;
+        r = (n1>n2)?n1+n2:n1-n2;
+        System.out.println(r);
+    }
+    
+}
