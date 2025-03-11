cfa279b4ba23cdf1a966a03829bc322a06d399ae
yago <yago.diasoliveira16@gmail.com>
2025-03-11 14:34:38 -0300

    Switch Case em Java

---
+++ java/java021 SWITCH CASE/src/java021/pkgswitch/pkgcase/Java021SWITCHCASE.java
@@ -0,0 +1,25 @@
+package java021.pkgswitch.pkgcase;
+
+public class Java021SWITCHCASE {
+
+    public static void main(String[] args) {
+        String sigla = "P";
+        switch (sigla) {
+            case "P": {
+                System.out.println("PEQUENO");
+                break;
+            }
+            case "M": {
+                System.out.println("PEQUENO");
+                break;
+            }
+            case "G": {
+                System.out.println("PEQUENO");
+                break;
+            }
+            default:
+                System.out.println("TAMANHO NÃO ENCONTRADO");
+        }
+    }
+    
+}
